79b7c620114a20a0bddec532ac27904358ed5fd3
Nils Christian Haugen <nch@scienta.no>
2025-03-27 17:52:59 +0100

    Added colour settings page

---
+++ src/main/kotlin/io/github/nchaugen/tabletest/language/TableTestColorSettingsPage.kt
@@ -0,0 +1,37 @@
+package io.github.nchaugen.tabletest.language
+
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import com.intellij.openapi.options.colors.ColorDescriptor
+import com.intellij.openapi.options.colors.ColorSettingsPage
+
+class TableTestColorSettingsPage : ColorSettingsPage {
+    override fun getIcon() = TableTestIcons.FILE
+
+    override fun getHighlighter() = TableTestSyntaxHighlighter()
+
+    override fun getDemoText() = """
+        Value | List             | Result?
+        123   | [10, 20, 30]     | [a: 133, b: 143, c: 153]
+        ""    | []               | [:]
+        abc   | [10, '20', "30"] | [a: [1, 2, 3], b: []]
+        ]     | [,               | [:
+    """.trimIndent()
+
+    override fun getAdditionalHighlightingTagToDescriptorMap() = null
+
+    override fun getAttributeDescriptors() =
+        arrayOf<AttributesDescriptor>(
+            AttributesDescriptor("Column input header", TableTestSyntaxHighlighter.INPUT_HEADER_KEY),
+            AttributesDescriptor("Column output header", TableTestSyntaxHighlighter.OUTPUT_HEADER_KEY),
+            AttributesDescriptor("Column separator", TableTestSyntaxHighlighter.COLUMN_SEPARATOR_KEY),
+            AttributesDescriptor("Value", TableTestSyntaxHighlighter.VALUE_KEY),
+            AttributesDescriptor("Quote", TableTestSyntaxHighlighter.QUOTE_KEY),
+            AttributesDescriptor("List and map punctuation", TableTestSyntaxHighlighter.LIST_PUNCTUATION_KEY),
+            AttributesDescriptor("Map key", TableTestSyntaxHighlighter.MAP_KEY_KEY),
+            AttributesDescriptor("Bad value", TableTestSyntaxHighlighter.BAD_CHARACTER_KEY)
+        )
+
+    override fun getColorDescriptors(): Array<out ColorDescriptor?> = ColorDescriptor.EMPTY_ARRAY
+
+    override fun getDisplayName() = "TableTest"
+}
--- src/main/kotlin/io/github/nchaugen/tabletest/language/TableTestSyntaxHighlighter.kt
+++ src/main/kotlin/io/github/nchaugen/tabletest/language/TableTestSyntaxHighlighter.kt
@@ -28,47 +28,35 @@ class TableTestSyntaxHighlighter : SyntaxHighlighterBase() {
 
     override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey?> =
         when (tokenType) {
-            INPUT_HEADER -> INPUT_HEADER_KEYS
-            OUTPUT_HEADER -> OUTPUT_HEADER_KEYS
-            PIPE -> COLUMN_SEPARATOR_KEYS
-            DOUBLE_QUOTE, SINGLE_QUOTE -> QUOTE_KEYS
-            MAP_KEY -> MAP_KEY_KEYS
-            LEFT_BRACKET, RIGHT_BRACKET, EMPTY_MAP, COMMA, COLON -> LIST_KEYS
-            BAD_CHARACTER -> BAD_CHAR_KEYS
-            STRING_VALUE -> STRING_KEYS
-            else -> EMPTY_KEYS
+            INPUT_HEADER -> arrayOf(INPUT_HEADER_KEY)
+            OUTPUT_HEADER -> arrayOf(OUTPUT_HEADER_KEY)
+            PIPE -> arrayOf(COLUMN_SEPARATOR_KEY)
+            DOUBLE_QUOTE, SINGLE_QUOTE -> arrayOf(QUOTE_KEY)
+            MAP_KEY -> arrayOf(MAP_KEY_KEY)
+            LEFT_BRACKET, RIGHT_BRACKET, EMPTY_MAP, COMMA, COLON -> arrayOf(LIST_PUNCTUATION_KEY)
+            BAD_CHARACTER -> arrayOf(BAD_CHARACTER_KEY)
+            STRING_VALUE -> arrayOf(VALUE_KEY)
+            else -> arrayOf()
         }
 
     companion object {
-        val INPUT_HEADER_KEY: TextAttributesKey? =
-            createTextAttributesKey("TABLE_TEST_INPUT_HEADER", DefaultLanguageHighlighterColors.INSTANCE_METHOD)
-        val OUTPUT_HEADER_KEY: TextAttributesKey? =
-            createTextAttributesKey("TABLE_TEST_OUTPUT_HEADER", DefaultLanguageHighlighterColors.STATIC_METHOD)
-        val MAP_KEY_KEY: TextAttributesKey? =
-            createTextAttributesKey("TABLE_TEST_MAP_KEY", DefaultLanguageHighlighterColors.KEYWORD)
-        val SEPARATOR_KEY: TextAttributesKey? =
-            createTextAttributesKey("TABLE_TEST_COLUMN_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN)
-        val LIST_KEY: TextAttributesKey? =
-            createTextAttributesKey("TABLE_TEST_LIST", DefaultLanguageHighlighterColors.BRACKETS)
-        val STRING_KEY: TextAttributesKey? =
-            createTextAttributesKey("TABLE_TEST_STRING", DefaultLanguageHighlighterColors.STRING)
-        val QUOTES_KEY: TextAttributesKey? =
-            createTextAttributesKey("TABLE_TEST_QUOTES", DefaultLanguageHighlighterColors.PARENTHESES)
-        val COMMENT_KEY: TextAttributesKey? =
+        val INPUT_HEADER_KEY: TextAttributesKey =
+            createTextAttributesKey("TABLE_TEST_INPUT_HEADER", DefaultLanguageHighlighterColors.STATIC_FIELD)
+        val OUTPUT_HEADER_KEY: TextAttributesKey =
+            createTextAttributesKey("TABLE_TEST_OUTPUT_HEADER", DefaultLanguageHighlighterColors.STATIC_FIELD)
+        val MAP_KEY_KEY: TextAttributesKey =
+            createTextAttributesKey("TABLE_TEST_MAP_KEY", DefaultLanguageHighlighterColors.STRING)
+        val COLUMN_SEPARATOR_KEY: TextAttributesKey =
+            createTextAttributesKey("TABLE_TEST_COLUMN_SEPARATOR", DefaultLanguageHighlighterColors.KEYWORD)
+        val LIST_PUNCTUATION_KEY: TextAttributesKey =
+            createTextAttributesKey("TABLE_TEST_LIST_PUNCTUATION", DefaultLanguageHighlighterColors.IDENTIFIER)
+        val VALUE_KEY: TextAttributesKey =
+            createTextAttributesKey("TABLE_TEST_VALUE", DefaultLanguageHighlighterColors.IDENTIFIER)
+        val QUOTE_KEY: TextAttributesKey =
+            createTextAttributesKey("TABLE_TEST_QUOTE", DefaultLanguageHighlighterColors.IDENTIFIER)
+        val COMMENT_KEY: TextAttributesKey =
             createTextAttributesKey("TABLE_TEST_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
-        val BAD_CHARACTER_KEY: TextAttributesKey? =
+        val BAD_CHARACTER_KEY: TextAttributesKey =
             createTextAttributesKey("TABLE_TEST_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
-
-
-        private val BAD_CHAR_KEYS = arrayOf<TextAttributesKey?>(BAD_CHARACTER_KEY)
-        private val INPUT_HEADER_KEYS = arrayOf<TextAttributesKey?>(INPUT_HEADER_KEY)
-        private val OUTPUT_HEADER_KEYS = arrayOf<TextAttributesKey?>(OUTPUT_HEADER_KEY)
-        private val MAP_KEY_KEYS = arrayOf<TextAttributesKey?>(MAP_KEY_KEY)
-        private val COLUMN_SEPARATOR_KEYS = arrayOf<TextAttributesKey?>(SEPARATOR_KEY)
-        private val LIST_KEYS = arrayOf<TextAttributesKey?>(LIST_KEY)
-        private val STRING_KEYS = arrayOf<TextAttributesKey?>(STRING_KEY)
-        private val QUOTE_KEYS = arrayOf<TextAttributesKey?>(QUOTES_KEY)
-        private val COMMENT_KEYS = arrayOf<TextAttributesKey?>(COMMENT_KEY)
-        private val EMPTY_KEYS = arrayOf<TextAttributesKey?>()
     }
 }
