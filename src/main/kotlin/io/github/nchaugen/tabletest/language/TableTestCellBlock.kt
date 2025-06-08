269bbe3065209b5654726f482a5baee7d5cb80fe
Nils Christian Haugen <nch@scienta.no>
2025-06-08 12:17:58 +0200

    Added autoformatting of compound value elements

--- src/main/kotlin/io/github/nchaugen/tabletest/language/TableTestCellBlock.kt
+++ src/main/kotlin/io/github/nchaugen/tabletest/language/TableTestCellBlock.kt
@@ -6,7 +6,9 @@ import com.intellij.formatting.Indent
 import com.intellij.formatting.Spacing
 import com.intellij.formatting.SpacingBuilder
 import com.intellij.formatting.Wrap
+import com.intellij.formatting.WrapType
 import com.intellij.lang.ASTNode
+import com.intellij.psi.TokenType
 import com.intellij.psi.formatter.common.AbstractBlock
 
 class TableTestCellBlock(
@@ -16,12 +18,29 @@ class TableTestCellBlock(
     val spacingBuilder: SpacingBuilder?
 ) : AbstractBlock(node, wrap, alignment) {
 
-    override fun buildChildren(): List<Block> = emptyList()
+    override fun buildChildren(): List<Block> {
+        val blocks: MutableList<Block> = ArrayList()
+        var child = node.firstChildNode
+
+        while (child != null) {
+            if (child.elementType != TokenType.WHITE_SPACE) {
+                val block = TableTestCellBlock(
+                    child,
+                    Wrap.createWrap(WrapType.NONE, false),
+                    null,
+                    spacingBuilder
+                )
+                blocks.add(block)
+            }
+            child = child.treeNext
+        }
+        return blocks
+    }
 
     override fun getSpacing(child1: Block?, child2: Block): Spacing? =
         spacingBuilder?.getSpacing(this, child1, child2)
 
-    override fun isLeaf(): Boolean = true
+    override fun isLeaf(): Boolean = node.firstChildNode == null
 
     override fun getIndent(): Indent? = Indent.getNoneIndent()
 }
--- src/main/kotlin/io/github/nchaugen/tabletest/language/TableTestFormattingModelBuilder.kt
+++ src/main/kotlin/io/github/nchaugen/tabletest/language/TableTestFormattingModelBuilder.kt
@@ -8,9 +8,11 @@ import com.intellij.formatting.SpacingBuilder
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import io.github.nchaugen.tabletest.language.psi.TableTestTypes.COLON
 import io.github.nchaugen.tabletest.language.psi.TableTestTypes.COMMA
+import io.github.nchaugen.tabletest.language.psi.TableTestTypes.LEFT_BRACE
 import io.github.nchaugen.tabletest.language.psi.TableTestTypes.LEFT_BRACKET
 import io.github.nchaugen.tabletest.language.psi.TableTestTypes.LINE_COMMENT
 import io.github.nchaugen.tabletest.language.psi.TableTestTypes.PIPE
+import io.github.nchaugen.tabletest.language.psi.TableTestTypes.RIGHT_BRACE
 import io.github.nchaugen.tabletest.language.psi.TableTestTypes.RIGHT_BRACKET
 
 class TableTestFormattingModelBuilder : FormattingModelBuilder {
@@ -42,4 +44,6 @@ class TableTestFormattingModelBuilder : FormattingModelBuilder {
             .after(COMMA).spaces(1)
             .after(LEFT_BRACKET).spaces(0)
             .before(RIGHT_BRACKET).spaces(0)
+            .after(LEFT_BRACE).spaces(0)
+            .before(RIGHT_BRACE).spaces(0)
 }
