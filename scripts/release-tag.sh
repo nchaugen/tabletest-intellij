6d04ac0072e7e5f1f235bd803ffefb72297c05a1
Nils Christian Haugen <nch@scienta.no>
2026-01-16 16:47:45 +0100

    feat: Add changelog update to release script

--- scripts/release-tag.sh
+++ scripts/release-tag.sh
@@ -2,6 +2,7 @@
 set -e
 
 GRADLE_PROPERTIES="gradle.properties"
+CHANGELOG="CHANGELOG.md"
 
 # Read current version from gradle.properties
 current_version=$(grep "^pluginVersion=" "$GRADLE_PROPERTIES" | cut -d'=' -f2)
@@ -10,8 +11,9 @@ if [ -z "$1" ]; then
     # No argument - use version from gradle.properties
     version="$current_version"
     echo "Using version from gradle.properties: $version"
+    updated_files=false
 else
-    # Argument provided - update gradle.properties
+    # Argument provided - update gradle.properties and changelog
     version="$1"
     echo "Updating version from $current_version to $version"
 
@@ -20,8 +22,20 @@ else
     else
         sed -i "s/^pluginVersion=.*/pluginVersion=$version/" "$GRADLE_PROPERTIES"
     fi
-
     echo "Updated gradle.properties"
+
+    # Update changelog - insert version header after [Unreleased]
+    release_date=$(date +%Y-%m-%d)
+    version_header="## [$version] - $release_date"
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "s/^## \[Unreleased\]$/## [Unreleased]\n\n$version_header/" "$CHANGELOG"
+    else
+        sed -i "s/^## \[Unreleased\]$/## [Unreleased]\n\n$version_header/" "$CHANGELOG"
+    fi
+    echo "Updated CHANGELOG.md with $version_header"
+
+    updated_files=true
 fi
 
 tag="v$version"
@@ -43,9 +57,8 @@ read -p "Create tag $tag? (y/n) " -n 1 -r
 echo ""
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    if [ -n "$1" ]; then
-        # If we updated the version, commit the change first
-        git add "$GRADLE_PROPERTIES"
+    if [ "$updated_files" = true ]; then
+        git add "$GRADLE_PROPERTIES" "$CHANGELOG"
         git commit -m "Bump version to $version"
     fi
 
@@ -56,10 +69,9 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "To push the tag and trigger the release workflow:"
     echo "  git push origin main && git push origin $tag"
 else
-    if [ -n "$1" ]; then
-        # Revert the gradle.properties change
-        git checkout "$GRADLE_PROPERTIES"
-        echo "Reverted gradle.properties"
+    if [ "$updated_files" = true ]; then
+        git checkout "$GRADLE_PROPERTIES" "$CHANGELOG"
+        echo "Reverted changes"
     fi
     echo "Aborted"
 fi
